57f9164941e1758124c955792ee927479d0b53ab
Devashi Tandon <dtandon2450@columbia.edu>
2014-10-14 16:53:14 -0400

    Some more checkpatch errors

--- flo-kernel/kernel/acceleration.c
+++ flo-kernel/kernel/acceleration.c
@@ -12,13 +12,13 @@
 static atomic_t counter = ATOMIC_INIT(0);
 static DECLARE_WAIT_QUEUE_HEAD(__queue);
 /*
- * Set current device acceleration in the kernel.
- * The parameter acceleration is the pointer to the address
- * where the sensor data is stored in user space.  Follow system call
- * convention to return 0 on success and the appropriate error value
- * on failure.
- * syscall number 378
- */
+* Set current device acceleration in the kernel.
+* The parameter acceleration is the pointer to the address
+* where the sensor data is stored in user space.  Follow system call
+* convention to return 0 on success and the appropriate error value
+* on failure.
+* syscall number 378
+*/
 
 SYSCALL_DEFINE1(set_acceleration,
 		struct dev_acceleration __user *, acceleration)
@@ -75,10 +75,10 @@ SYSCALL_DEFINE1(set_acceleration,
 
 
 /* Create an event based on motion.
- * If frq exceeds WINDOW, cap frq at WINDOW.
- * Return an event_id on success and the appropriate error on failure.
- * system call number 379
- */
+* If frq exceeds WINDOW, cap frq at WINDOW.
+* Return an event_id on success and the appropriate error on failure.
+* system call number 379
+*/
 
 SYSCALL_DEFINE1(accevt_create, struct acc_motion __user *, acceleration)
 {
@@ -112,10 +112,10 @@ SYSCALL_DEFINE1(accevt_create, struct acc_motion __user *, acceleration)
 }
 
 /* Block a process on an event.
- * It takes the event_id as parameter. The event_id requires verification.
- * Return 0 on success and the appropriate error on failure.
- * system call number 380
- */
+* It takes the event_id as parameter. The event_id requires verification.
+* Return 0 on success and the appropriate error on failure.
+* system call number 380
+*/
 
 SYSCALL_DEFINE1(accevt_wait, int, event_id)
 {
@@ -155,13 +155,13 @@ SYSCALL_DEFINE1(accevt_wait, int, event_id)
 
 
 /* The acc_signal system call
- * takes sensor data from user, stores the data in the kernel,
- * generates a motion calculation, and notify all open events whose
- * baseline is surpassed.  All processes waiting on a given event
- * are unblocked.
- * Return 0 success and the appropriate error on failure.
- * system call number 381
- */
+* takes sensor data from user, stores the data in the kernel,
+* generates a motion calculation, and notify all open events whose
+* baseline is surpassed.  All processes waiting on a given event
+* are unblocked.
+* Return 0 success and the appropriate error on failure.
+* system call number 381
+*/
 
 SYSCALL_DEFINE1(accevt_signal, struct dev_acceleration __user *, acceleration)
 {
@@ -232,9 +232,9 @@ SYSCALL_DEFINE1(accevt_signal, struct dev_acceleration __user *, acceleration)
 }
 
 /* Destroy an acceleration event using the event_id,
- * Return 0 on success and the appropriate error on failure.
- * system call number 382
- */
+* Return 0 on success and the appropriate error on failure.
+* system call number 382
+*/
 
 SYSCALL_DEFINE1(accevt_destroy, int, event_id)
 {
@@ -245,8 +245,8 @@ SYSCALL_DEFINE1(accevt_destroy, int, event_id)
 		write_lock(&lock_event);
 		event_to_destroy = get_event_using_id(event_id);
 		/*waking up processes waiting on this event
-		but normal_wakeup not set hence processes will
-		not print shake detected*/
+		* but normal_wakeup not set hence processes will
+		* not print shake detected*/
 		if (event_to_destroy == NULL) {
 			write_unlock(&lock_event);
 			pr_err("accevt_destroy: Error");
